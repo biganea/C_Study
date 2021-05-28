7dcf167945b6727ec56478e93dfe3df30253d260
biganea <rlatjdgh1236@gmail.com>
2021-05-29 07:46:56 +0900

    set

--- use3.c
+++ use3.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 
 int main(void){
@@ -15,25 +15,4 @@ int main(void){
 	}
 
 	printf("%d",a%10);
-	
-=======
-#include <stdio.h>
-
-int main(void){
-	
-	int input1[5];
-	int a,b,c;
-	a = 0;
-	for(int i = 0;i<5;i++){
-		scanf("%d",&input1[i]);
-	}
-	
-	for(int i = 0;i<5;i++){
-		a = a + input1[i] * input1[i];
-	}
-
-	printf("%d",a%10);
-	printf("yeas");
-	
->>>>>>> 313863b4b7c733e7e6449ff6f14fa341662d37f0
 }

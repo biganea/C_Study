17e510bb0a19872119c162af103bfe523d7b64a7
nananu <sshh215@naver.com>
2021-06-01 05:58:22 +0000

    test

--- use1.c
+++ use1.c
@@ -1,16 +1,79 @@
 #include <stdio.h>
-
-struct Person {
-	int age;
-	int hight;
-	int kg;
-};
-
+#include <string.h>
+#define N_MAX_SIZE 10000
+#define input_MAX_SIZE 10000
+int set = 0;
+int cut = 0;
+int prt[N_MAX_SIZE] = {-1,};
+int push(int stack[N_MAX_SIZE],int input1){
+	stack[set] = input1;
+	set++;
+}
+int pop(int stack[N_MAX_SIZE]){
+	if (set-1 < 0){
+		prt[cut] = -1;
+		cut++;
+	}
+	else{
+		prt[cut] = stack[set-1];
+		cut++;
+		stack[set-1] = -1;
+		set--;
+		}
+}
+int size(int stack[N_MAX_SIZE]){
+	printf("%d\n",set);
+}
+int empty(int stack[N_MAX_SIZE]){
+	if (stack[0] == -1){
+		prt[cut] = 1;
+		cut++;
+	}
+	else{
+		prt[cut] = 0;
+		cut++;
+	}
+}
+int top(int stack[N_MAX_SIZE]){
+	if (set-1 < 0){
+		prt[cut] = -1;
+		cut++;
+	}
+	else {
+		prt[cut] = stack[set-1];
+		cut++;
+	}
+}
 int main(void){
-	struct Person p1;
-
-	p1.age = 24;
-
-	printf("%d",p1.age);
-
+	
+	int scan1,i;
+	char scan2[30];
+	int scan3;
+	int stack[N_MAX_SIZE] = {-1,};
+	
+	scanf("%d",&scan1);
+	while (getchar() != '\n');
+	for(i=0;i<scan1;i++){
+		scanf("%s",scan2);
+		if(strcmp(scan2,"push") == 0){
+			scanf("%d",&scan3);
+			push(stack,scan3);
+		}
+		else if(strcmp(scan2,"top") == 0){
+			top(stack);
+		}
+		else if(strcmp(scan2,"pop") == 0){
+			pop(stack);
+		}
+		else if(strcmp(scan2,"pop") == 0){
+			size(stack);
+		}
+		else if(strcmp(scan2,"pop") == 0){
+			empty(stack);
+		}
+	}
+	
+	for(i=0;i<cut;i++){
+		printf("%d\n",prt[i]);
+	}
 }
--- use2.c
+++ use2.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
 	
+	int scan1,i;
+	char scan2[30];
+	int scan3;
+	int stack[10] = {-1,};
 	
-	int nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn;
-	
-	printf("hello wolrd2");
-	scanf("%d",&nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn);
-	
-	printf("%d",nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn*2);
-	
+	scanf("%d",&scan1);
+	while (getchar() != '\n');
+	scanf("%s",scan2);
+	if(strcmp(scan2,"push") == 0){
+		scanf("%d",&scan3);
+	}
+	printf("%s\n",scan2);
+	printf("%d\n",scan3);
 }
 

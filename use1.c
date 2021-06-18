73f8b2050efb9559647c55591bd1a4ede5c608e1
biganea <rlatjdgh1236@gmail.com>
2021-06-18 16:45:42 +0900

    test1

--- use1.c
+++ use1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 
 typedef struct HNODE{
 	int cnt;
@@ -76,39 +75,4 @@ int main(void){
 
 	scanf("%d",&input);
 	get_num(input);
-=======
-#include <stdlib.h>
-#include <time.h>
- 
-int main(void) 
-{
-	char input1[10];
-	int win,loss,draw;
-	win = 0;
-	loss = 0;
-	draw = 0;
-	while(1){
-		
-		printf("가위바위보 : ");
-		scanf("%s",input1);
-		
-		srand(time(NULL));
-		int random = rand() % 3;
-		
-		if(random==0){
-			printf("승리\n");
-			win++;
-		}
-		else if(random==1){
-			printf("패배\n");
-			loss++;
-			break;
-		}
-		else if(random==2){
-			printf("무승부\n");
-			draw++;
-		}
-	}
-	printf("%d승 %d패 %d무",win,loss,draw);
->>>>>>> e4d5a5816a61f0919ba2c5bcfcd1a03393525ab5
 }
